5bc5a37f63401a955b56a30f11e349b673dfd3e8
Paul Griffiths <paul@paulgriffiths.net>
2020-03-07 15:13:00 -0500

    Add more OIDs.

--- asn1/oids.go
+++ asn1/oids.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-// OID constants.
+// Extension OID values.
 var (
 	OIDSubjectKeyIdentifier   = goasn1.ObjectIdentifier{2, 5, 29, 14}
 	OIDKeyUsage               = goasn1.ObjectIdentifier{2, 5, 29, 15}
@@ -14,6 +14,28 @@ var (
 	OIDBasicConstraints       = goasn1.ObjectIdentifier{2, 5, 29, 19}
 	OIDAuthorityKeyIdentifier = goasn1.ObjectIdentifier{2, 5, 29, 35}
 	OIDExtendedKeyUsage       = goasn1.ObjectIdentifier{2, 5, 29, 37}
+)
+
+// Signature and hash OID values.
+var (
+	OIDSignatureMD2WithRSA      = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 2}
+	OIDSignatureMD5WithRSA      = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 4}
+	OIDSignatureSHA1WithRSA     = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 5}
+	OIDSignatureSHA256WithRSA   = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 11}
+	OIDSignatureSHA384WithRSA   = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 12}
+	OIDSignatureSHA512WithRSA   = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 13}
+	OIDSignatureRSAPSS          = goasn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 10}
+	OIDSignatureDSAWithSHA1     = goasn1.ObjectIdentifier{1, 2, 840, 10040, 4, 3}
+	OIDSignatureDSAWithSHA256   = goasn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 3, 2}
+	OIDSignatureECDSAWithSHA1   = goasn1.ObjectIdentifier{1, 2, 840, 10045, 4, 1}
+	OIDSignatureECDSAWithSHA256 = goasn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 2}
+	OIDSignatureECDSAWithSHA384 = goasn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 3}
+	OIDSignatureECDSAWithSHA512 = goasn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 4}
+	OIDSignatureEd25519         = goasn1.ObjectIdentifier{1, 3, 101, 112}
+
+	OIDSHA256 = goasn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 1}
+	OIDSHA384 = goasn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 2}
+	OIDSHA512 = goasn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 3}
 )
 
 // ParseOID parses a dotted decimal string representation of an OID.
